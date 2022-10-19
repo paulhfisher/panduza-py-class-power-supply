3daea3d123d4d9ff935533be4ed5a5eb5c1a7af6
paul <paul.fisher@elsys-design.com>
2022-10-19 09:49:43 +0200

    back to version 0.0.1 and delete DriverQL335P

--- panduza-py-class-power-supply/__init__.py
+++ panduza-py-class-power-supply/__init__.py
@@ -1,12 +1,11 @@
 import imp
 from .driver_psu_hm7044 import DriverPsuHm7044
 from .driver_ka005p import DriverKA005P
-from .driver_ql335p import DriverQL335P
 
 PZA_DRIVERS_LIST=[
     DriverPsuHm7044,
     DriverKA005P,
-    DriverQL335P
+    
 ]
 
 
